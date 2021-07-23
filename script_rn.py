fe3e32f7316243fda986021b124724409bd0f518
Camilo <milocosta@yahoo.com.br>
2021-07-23 19:48:45 -0300

    Teste RN - novo arquivo

---
+++ script_rn.py
@@ -0,0 +1,9 @@
+# %% Importação das bibliotecas básicas
+import numpy as np
+import random as rd
+import pandas as pd
+import missingno as msno
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+# %%
